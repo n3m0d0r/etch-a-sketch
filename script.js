88358cb844bfe899f03090e426024cf1277bc246
n3m0d0r <137563305+n3m0d0r@users.noreply.github.com>
2023-07-19 23:35:19 -0300

    Fix small rgb typo

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function generateRandomRgb() {
   const red = Math.round(Math.random() * 255);
   const green = Math.round(Math.random() * 255);
   const blue = Math.round(Math.random() * 255);
-  return `rgb(${red}, ${green}, ${blue}`;
+  return `rgb(${red}, ${green}, ${blue})`;
 }
 
 function updateGrid() {
