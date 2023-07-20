1cb9001db5c9e280557ee46b430d6364908e3475
n3m0d0r <137563305+n3m0d0r@users.noreply.github.com>
2023-07-19 23:03:59 -0300

    Complete Assignment

---
+++ script.js
@@ -0,0 +1,47 @@
+const app = document.querySelector('#app');
+
+const sizeButton = document.createElement('button');
+sizeButton.textContent = 'new canvas';
+sizeButton.addEventListener('click', updateGrid);
+app.appendChild(sizeButton);
+
+const canvas = document.createElement('div');
+canvas.classList.add('canvas');
+app.appendChild(canvas);
+
+function updateGrid() {
+  cleanCanvas();
+  let gridSize = getGridSize();
+  generateGrid(gridSize);
+}
+
+function cleanCanvas() {
+  canvas.innerHTML = '';
+}
+
+function getGridSize() {
+  let gridSize = null;
+  while (!gridSize || gridSize > 100) {
+    gridSize = Number(prompt('enter the number of squares per side: (maximum 100)'));
+  }
+  return gridSize;
+}
+
+function generateGrid(fullGridSize) {
+  for (let i = 0; i < fullGridSize; i++) {
+    let row = document.createElement('div');
+    row.classList.add('row');
+    for(let j = 0; j < fullGridSize; j++) {
+      const square = document.createElement('div');
+      square.classList.add(`square`);
+      square.addEventListener('mouseover', changeColor);
+      row.appendChild(square);
+    }
+    canvas.appendChild(row);
+  }
+}
+
+function changeColor(e) {
+  e.target.style.backgroundColor = 'blue';
+}
+
